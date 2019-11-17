c29f7889c73efbcfb7b245aea758f113a74ee36f
Dencher12 <deischerry19@gmail.com>
2019-11-17 19:55:53 +0300

    Добавил letter_opener

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,6 @@
 class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
-  belongs_to :route, optional: true
+  belongs_to :route
   has_many :tickets
   has_many :cars
 
