da0b91249083bfceed4fd9ea67c02e684e0ed18f
Den <deischerry19@gmail.com>
2019-12-15 14:13:57 +0300

    Подключил cloudmailin

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'therubyracer'
 gem 'twitter-bootstrap-rails'
 
 gem 'rails_12factor'
+gem 'cloudmailin-rails3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: "infinite-chamber-18282.herokuapp.com"}
+  config.action_mailer.delivery_method = :cloudmailin
+  config.action_mailer.reply_to = env['CLOUDMAILIN_FORWARD_ADDRESS']
+
 end
